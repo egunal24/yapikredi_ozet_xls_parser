927a924c4db18c857c097a455938de29e40db942
egunal24 <egunal24@ku.edu.tr>
2026-04-09 14:00:09 +0300

    Working version 1

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from res import ExcelProcessor
 def main():
     e = ExcelProcessor("Hesap_Hareketleri.xls")
     e.filter(1000)
-    print(e.filtered_sum)
+    e.show()
 
 if __name__ == "__main__":
     main()
-
--- res.py
+++ res.py
@@ -1,4 +1,5 @@
 from pandas import read_excel
+import datetime
 
 class ExcelProcessor:
 
@@ -9,9 +10,27 @@ class ExcelProcessor:
 
         self.l = []
         self.filtered_sum = 0
-        print(self.money_col)
     
     def filter(self,scale):
         for money in self.money_col:
             if abs(money) <= scale: self.l.append(money)
         self.filtered_sum = sum(self.l)
+
+    def calculate_days(self):
+        date_col = self.df["Hesap Hareketleri"]
+        end_date = datetime.datetime.strptime(date_col[10],"%d/%m/%Y")
+        start_date = datetime.datetime.strptime(date_col[len(date_col)-1],"%d/%m/%Y")
+        date_diff = end_date - start_date
+        return date_diff.days
+
+    def show(self):
+        scale = float(input("Input a positive numerical value to filter transactions by:"))
+        self.filter(scale)
+        days = self.calculate_days()
+
+        print("---------------------------------------------------------------------")
+        print("------------------------TRANSACTIONAL HISTORY------------------------")
+        print("---------------------------------------------------------------------")
+        print(f"Total money delta in {days} days equals: {self.filtered_sum}")
+        print(f"Average money delta per day equals: {self.filtered_sum/days}")
+        print("---------------------------------------------------------------------")
